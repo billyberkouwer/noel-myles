6791a5c0e767a3b71ac787fbfb43e7de093addca
billy.berkouwer <billy@ocove.studio>
2023-09-02 11:54:31 +0100

    update: (Next) large-building-slpha-fixed

--- src/components/global/Model.tsx
+++ src/components/global/Model.tsx
@@ -1,7 +1,7 @@
 import { useGLTF, Clone } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { useEffect, useState } from "react";
-import { Mesh } from "three"
+import { Mesh, PCFSoftShadowMap } from "three"
 
 export default function Model({ url, id, position }: { url: string, id?: string, position?: [number, number, number] }) {
     const { scene } = useGLTF(url);
@@ -17,6 +17,7 @@ export default function Model({ url, id, position }: { url: string, id?: string,
         });
         gl.shadowMap.needsUpdate = true;
         gl.shadowMap.enabled = true;
+        gl.shadowMap.type = PCFSoftShadowMap;
     }, [scene, gl])
 
     return <primitive scale={[0.01,0.01,0.01]} object={scene} />
